af88b2826acbad7095666a57af39cb70db72e8bb
John <alexway124@gmail.com>
2024-05-18 19:38:49 +0300

    update files

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,12 @@
 #!/usr/bin/env node;
+import questionForUser from "../src/cli.js";
 
 (function(){
   console.log('Welcome to the Brain Games!');
+  const answer = questionForUser();
+  console.log(`My name is ${answer}`); 
+  if(answer){
+    console.log(`Hello, ${answer}!`);
+  }
 }()); 
+
