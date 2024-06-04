88fb0db7a160eddfaa18715d59c4a4d1634c14d9
John <alexway124@gmail.com>
2024-06-04 13:09:21 +0300

    correct files

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node;
+#!/usr/bin/env node
 
 import gameGCD from '../src/games/game-gcd.js';
 
--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node;
+#!/usr/bin/env node
 
 import gamePrime from '../src/games/game-prime.js';
 
